34ef54e4db3dbee98d7563f2f5ad9cb1779d71f4
jlbuild <juliabuildbot@gmail.com>
2024-06-20 04:19:46 +0000

    openfhe_julia_jll build 0.3.2+0

--- src/wrappers/aarch64-apple-darwin-julia_version+1.11.0.jl
+++ src/wrappers/aarch64-apple-darwin-julia_version+1.11.0.jl
@@ -10,7 +10,7 @@ function __init__()
     JLLWrappers.@generate_init_header(libcxxwrap_julia_jll, OpenFHE_jll, LLVMOpenMP_jll)
     JLLWrappers.@init_library_product(
         libopenfhe_julia,
-        "lib/libopenfhe_julia.0.3.1.dylib",
+        "lib/libopenfhe_julia.0.3.2.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
